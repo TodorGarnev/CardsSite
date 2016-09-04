ac892980eb22f24112331e22e10b7c2f7ffe1df9
Mihail Gaberov <mihail.gaberov@gmail.com>
2016-09-04 16:08:56 +0200

    Added img folder and external scripts for the slider.

---
+++ test-shopping-card/lib/slider.js
@@ -0,0 +1,12 @@
+ $(function() {
+      $('#slides').slidesjs({
+        width: 800,
+        height: 300,
+        play: {
+          active: true,
+          auto: true,
+          interval: 3000,
+          swap: true
+        }
+      });
+    });
