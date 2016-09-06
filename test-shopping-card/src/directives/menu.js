ddfb8dbcc8b159325a0447a50941b88d42e1a20a
Mihail Gaberov <mihail.gaberov@dreamit.de>
2016-09-06 18:27:51 +0200

    Added hover styles for the submenus.

--- test-shopping-card/src/directives/menu.js
+++ test-shopping-card/src/directives/menu.js
@@ -1,5 +1,19 @@
 myCardSiteControllers.controller('MenuCtrl',
-	function () {
+	function ($scope, $location) {
+		var vm = this;
+
+		vm.navTitles = {
+			home: 'home',
+			cards: 'cards-birthday',
+			flowers: 'flowers',
+			about: 'about',
+			contacts: 'contacts'
+		};
+
+		vm.navClass = function (page) {
+			var currentRoute = $location.path().substring(1) || 'home';
+			return page === currentRoute ? 'active' : '';
+		};
 
 	})
 	.directive('menu', function () {
