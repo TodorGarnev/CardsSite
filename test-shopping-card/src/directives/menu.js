d293e6973f022eff6307e3667845c92733bcb428
Mihail Gaberov <mihail.gaberov@dreamit.de>
2016-09-06 15:19:44 +0200

    Added "active" state for the menu items.

--- test-shopping-card/src/directives/menu.js
+++ test-shopping-card/src/directives/menu.js
@@ -1,5 +1,23 @@
 myCardSiteControllers.controller('MenuCtrl',
-	function () {
+	function ($scope, $location) {
+		var vm = this;
+
+		vm.navTitles = {
+			home: 'home',
+			cards: 'cards_birthday',
+			flowers: 'flowers',
+			about: 'about',
+			contacts: 'contacts'
+		};
+
+		vm.navClass = function (page) {
+			var currentRoute = $location.path().substring(1) || 'home';
+
+			console.log('>>> currentRoute: ', currentRoute);
+			console.log('>>> page: ', page);
+
+			return page === currentRoute ? 'active' : '';
+		};
 
 	})
 	.directive('menu', function () {
