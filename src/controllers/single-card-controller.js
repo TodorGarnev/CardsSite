119fd5616702f623d535f52843b127207f4b75ff
Mihail Gaberov <mihail.gaberov@gmail.com>
2016-09-10 00:10:00 +0200

    Fixed Back button to go one step back from browser history.

--- src/controllers/single-card-controller.js
+++ src/controllers/single-card-controller.js
@@ -1,9 +1,12 @@
 myCardSiteControllers.controller('SingleCardCtrl',
-	function ($rootScope, $scope, $routeParams, dbFactory, Events) {
+	function ($rootScope, $scope, $window, $routeParams, dbFactory, Events) {
     $scope.imgId = $routeParams.imgId;
     $scope.name = $routeParams.name;
     $scope.price = $routeParams.price;
 
+	$scope.back = function () {
+		$window.history.back();
+	};
 
 	$scope.addCard = function () {
 		var objCard = { name: $scope.name, price: $scope.price, quantity: $scope.quantity };
